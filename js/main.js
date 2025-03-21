5b73a4ae2d27c85323fccb4e457e6c58a5a3fff4
akamjot <sainiparam947@gmail.com>
2025-03-21 15:00:11 -0400

    changes made in js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,45 @@
-import {createApp} from vue;
+import { MusicBook } from './musicbook.js';
 
-const app = createApp({
-    data(){
-        return{
-            customerTotal: 0
-        };
-    },
+const musicBook = new MusicBook();
 
-    method:{
-        calculateTip(percentage){
-            return (this.customerTotal * percentage).toFixed(2);
-            
-        }
-    }
-}) .mount("#app");
+document.querySelector('#musicForm button').addEventListener('click', addMusic);
+function addMusic() {
+  const name = document.querySelector('#musicName').value;
+  const artist = document.querySelector('#musicArtist').value;
+  const releases = document.querySelector('#musicReleases').value;
+
+  musicBook.addMusic(name, artist, releases);
+
+  document.querySelector('#musicName').value = '';
+  document.querySelector('#musicArtist').value = '';
+  document.querySelector('#musicReleases').value = '';
+
+  // Render Musics
+  renderMusics();
+}
+
+// Function to render musics
+function renderMusics() {
+  const musicsElement = document.querySelector('#musics');
+  musicsElement.innerHTML = '';
+
+  musicBook.getMusics().forEach(music => {
+    const musicItem = document.createElement('li');
+
+    const musicNameHeading = document.createElement('h3');
+    musicNameHeading.textContent = music.name;
+    musicItem.appendChild(musicNameHeading);
+
+    const artistParagraph = document.createElement('p');
+    artistParagraph.textContent = 'Music Artist: ' + music.artist;
+    musicItem.appendChild(artistParagraph);
+
+    const releaseParagraph = document.createElement('p');
+    releaseParagraph.textContent = 'Music Release: ' + music.releases;
+    musicItem.appendChild(releaseParagraph);
+
+    musicsElement.appendChild(musicItem);
+  });
+}
+
+renderMusics();
